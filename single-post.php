8caabe34fe1a43ae16389d15f04d7a039d8e2a37
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2023-12-26 23:48:49 +0100

    Create Single post (blog) page

---
+++ single-post.php
@@ -0,0 +1,20 @@
+<?php
+get_header();
+
+
+// Get post data
+$title = get_the_title();
+$featured_image_url = has_post_thumbnail() ? get_the_post_thumbnail_url($blog_id, 'full') : get_template_directory_uri() . '/assets/images/gallery-1.jpg';
+$content = get_the_content();
+
+?>
+<img src="<?= $featured_image_url ?>" alt="<?= $title ?>" class="w-full h-[50vh] mt-20 object-cover">
+<div class="blog-content">
+    <div class="container mx-auto">
+        <h1 class="text-2xl text-cs-primary font-montserrat font-semibold mt-4 mb-6 lg:mt-8"><?= $title ?></h1>
+        <?= $content ?>
+    </div>
+</div>
+
+<?php
+get_footer();
